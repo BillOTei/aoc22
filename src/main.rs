21596d386d896ee4b55403ac6a222cecdef5ed7a
billo <ateilhet@gmail.com>
2022-12-02 17:38:34 +0100

    day 2 the child way

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,88 @@
+use std::env;
 use std::fs::File;
 use std::io::{prelude::*, BufReader, Result};
 
 fn main() -> Result<()> {
-    let file = File::open("/home/billo/aoc22/src/puzzles/01.txt")?;
+    let mut path = env::current_dir()?;
+    path.push("src/puzzles/02.txt");
+    let file = File::open(path)?;
     let reader = BufReader::new(file);
-    let mut r: u32 = 0;
-    let mut v: Vec<u32> = vec![];
 
-    for line in reader.lines() {
-        let maybe_c = line?.parse::<u32>();
-        if maybe_c.is_ok() {
-            r += maybe_c.unwrap();
-        } else {
-            v.push(r);
-            r = 0;
-        }
+    let mut r: u32 = 0;
+
+    for maybe_line in reader.lines() {
+        let line = maybe_line?;
+        let opp = &line[0..1];
+        let own = &line[2..];
+
+        r += play2(own, opp);
     }
 
-    v.sort();
+    Ok(println!("{}", r))
+}
 
-    let result_2: u32 = v.iter().rev().take(3).sum();
+fn play1(own: &str, opp: &str) -> u32 {
+    match opp {
+        "A" => {
+            if own == "X" {
+                3 + 1
+            } else if own == "Y" {
+                6 + 2
+            } else {
+                3
+            }
+        }
+        "B" => {
+            if own == "X" {
+                1
+            } else if own == "Y" {
+                3 + 2
+            } else {
+                6 + 3
+            }
+        }
+        "C" => {
+            if own == "X" {
+                6 + 1
+            } else if own == "Y" {
+                2
+            } else {
+                3 + 3
+            }
+        }
+        _ => 0,
+    }
+}
 
-    Ok(println!("{}", result_2))
+fn play2(own: &str, opp: &str) -> u32 {
+    match opp {
+        "A" => {
+            if own == "X" {
+                0 + 3
+            } else if own == "Y" {
+                3 + 1
+            } else {
+                6 + 2
+            }
+        }
+        "B" => {
+            if own == "X" {
+                0 + 1
+            } else if own == "Y" {
+                3 + 2
+            } else {
+                6 + 3
+            }
+        }
+        "C" => {
+            if own == "X" {
+                0 + 2
+            } else if own == "Y" {
+                3 + 3
+            } else {
+                6 + 1
+            }
+        }
+        _ => 0,
+    }
 }

11a31e5e28ea81a7770fb3ceefa098a8ba8677aa
billo <ateilhet@gmail.com>
2022-12-03 23:26:11 +0100

    Day 03

--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,71 @@
+use itertools::Itertools;
+use std::collections::HashSet;
 use std::env;
 use std::fs::File;
 use std::io::{prelude::*, BufReader, Result};
 
-fn main() -> Result<()> {
+pub(crate) fn main() -> Result<()> {
     let mut path = env::current_dir()?;
-    path.push("src/puzzles/02.txt");
+    path.push("src/puzzles/03.txt");
     let file = File::open(path)?;
     let reader = BufReader::new(file);
 
     let mut r: u32 = 0;
 
-    for maybe_line in reader.lines() {
-        let line = maybe_line?;
-        let opp = &line[0..1];
-        let own = &line[2..];
-
-        r += play2(own, opp);
+    // for maybe_line in reader.lines() {
+    //     let line = maybe_line?;
+    //     let i = line.len() / 2;
+    //     let s1 = &line[..i];
+    //     let s2 = &line[i..];
+    //     let a = 97;
+    //     let v = share_char1 (s1, s2) as u32;
+    //     if v >= a {
+    //         r += v - 96;
+    //     } else {
+    //         r += v - 38;
+    //     }
+    // }
+    for (a, b, c) in reader.lines().tuples() {
+        let (l1, l2, l3) = (a?, b?, c?);
+        let s1 = l1.as_str();
+        let s2 = l2.as_str();
+        let s3 = l3.as_str();
+        let v = share_char2(s1, s2, s3) as u32;
+        if v >= 97 {
+            r += v - 96;
+        } else {
+            r += v - 38;
+        }
     }
 
     Ok(println!("{}", r))
 }
 
-fn play1(own: &str, opp: &str) -> u32 {
-    match opp {
-        "A" => {
-            if own == "X" {
-                3 + 1
-            } else if own == "Y" {
-                6 + 2
-            } else {
-                3
-            }
+fn share_char1(a: &str, b: &str) -> char {
+    let (s1, s2) = (a, b);
+
+    let set: HashSet<char> = s1.chars().collect();
+
+    for c in s2.chars() {
+        if set.contains(&c) {
+            return c;
         }
-        "B" => {
-            if own == "X" {
-                1
-            } else if own == "Y" {
-                3 + 2
-            } else {
-                6 + 3
-            }
-        }
-        "C" => {
-            if own == "X" {
-                6 + 1
-            } else if own == "Y" {
-                2
-            } else {
-                3 + 3
-            }
-        }
-        _ => 0,
     }
+
+    'ç'
 }
 
-fn play2(own: &str, opp: &str) -> u32 {
-    match opp {
-        "A" => {
-            if own == "X" {
-                0 + 3
-            } else if own == "Y" {
-                3 + 1
-            } else {
-                6 + 2
-            }
+fn share_char2(a: &str, b: &str, c: &str) -> char {
+    let (s1, s2, s3) = (a, b, c);
+
+    let set: HashSet<char> = s1.chars().collect();
+    let set2: HashSet<char> = s2.chars().collect();
+
+    for c in s3.chars() {
+        if set.contains(&c) && set2.contains(&c) {
+            return c;
         }
-        "B" => {
-            if own == "X" {
-                0 + 1
-            } else if own == "Y" {
-                3 + 2
-            } else {
-                6 + 3
-            }
-        }
-        "C" => {
-            if own == "X" {
-                0 + 2
-            } else if own == "Y" {
-                3 + 3
-            } else {
-                6 + 1
-            }
-        }
-        _ => 0,
     }
+
+    'ç'
 }

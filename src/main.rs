c87133d227788d7606ea28bea3362c885567dfe2
billo <ateilhet@gmail.com>
2022-12-04 21:25:40 +0100

    Day 04

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,47 @@
 use itertools::Itertools;
-use std::collections::HashSet;
+use regex::Regex;
 use std::env;
 use std::fs::File;
 use std::io::{prelude::*, BufReader, Result};
 
 pub(crate) fn main() -> Result<()> {
     let mut path = env::current_dir()?;
-    path.push("src/puzzles/03.txt");
+    path.push("src/puzzles/04.txt");
     let file = File::open(path)?;
     let reader = BufReader::new(file);
 
     let mut r: u32 = 0;
+    let re = Regex::new(r"\d+").unwrap();
 
-    // for maybe_line in reader.lines() {
-    //     let line = maybe_line?;
-    //     let i = line.len() / 2;
-    //     let s1 = &line[..i];
-    //     let s2 = &line[i..];
-    //     let a = 97;
-    //     let v = share_char1 (s1, s2) as u32;
-    //     if v >= a {
-    //         r += v - 96;
-    //     } else {
-    //         r += v - 38;
-    //     }
-    // }
-    for (a, b, c) in reader.lines().tuples() {
-        let (l1, l2, l3) = (a?, b?, c?);
-        let s1 = l1.as_str();
-        let s2 = l2.as_str();
-        let s3 = l3.as_str();
-        let v = share_char2(s1, s2, s3) as u32;
-        if v >= 97 {
-            r += v - 96;
-        } else {
-            r += v - 38;
+    for maybe_line in reader.lines() {
+        let line = maybe_line?;
+        let matches = re.find_iter(&line);
+        for (m1, m2, m3, m4) in matches.tuples() {
+            let (l1, l2, l3, l4) = (m1.as_str(), m2.as_str(), m3.as_str(), m4.as_str());
+            let (s1, s2, s3, s4): (u32, u32, u32, u32) = (
+                l1.parse().unwrap(),
+                l2.parse().unwrap(),
+                l3.parse().unwrap(),
+                l4.parse().unwrap(),
+            );
+            if overlap(s1, s2, s3, s4) {
+                r += 1;
+            }
         }
     }
 
     Ok(println!("{}", r))
 }
 
-fn share_char1(a: &str, b: &str) -> char {
-    let (s1, s2) = (a, b);
-
-    let set: HashSet<char> = s1.chars().collect();
-
-    for c in s2.chars() {
-        if set.contains(&c) {
-            return c;
-        }
-    }
-
-    'ç'
+fn included(s1: u32, s2: u32, s3: u32, s4: u32) -> bool {
+    s1 <= s3 && s2 >= s4 || s1 >= s3 && s2 <= s4
 }
 
-fn share_char2(a: &str, b: &str, c: &str) -> char {
-    let (s1, s2, s3) = (a, b, c);
+fn overlap(s1: u32, s2: u32, s3: u32, s4: u32) -> bool {
+    let v1 = vec![s1, s3];
+    let v2 = vec![s2, s4];
+    let i1 = v1.iter().max();
+    let i2 = v2.iter().min();
 
-    let set: HashSet<char> = s1.chars().collect();
-    let set2: HashSet<char> = s2.chars().collect();
-
-    for c in s3.chars() {
-        if set.contains(&c) && set2.contains(&c) {
-            return c;
-        }
-    }
-
-    'ç'
+    i1.unwrap() <= i2.unwrap()
 }

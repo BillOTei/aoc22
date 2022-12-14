f8d74b77e67b47325893fa1408026f76bef17e14
billo <ateilhet@gmail.com>
2022-12-14 17:40:34 +0100

    Day 12

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,60 @@
 // use itertools::Itertools;
 // use std::collections::{BTreeMap, HashMap};
-use std::collections::HashSet;
+use pathfinding::dijkstra;
 use std::env;
 use std::fs::File;
 use std::io::{prelude::*, BufReader, Result};
-use std::ops::{Add, Sub};
+
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+struct Pos(i32, i32, i32);
 
 pub(crate) fn main() -> Result<()> {
     let mut path = env::current_dir()?;
-    path.push("src/puzzles/09.txt");
+    path.push("src/puzzles/12.txt");
     let file = File::open(path)?;
     let reader = BufReader::new(file);
     //let str_input = fs::read_to_string(path)?;
+    let grid: Vec<Vec<Pos>> = reader
+        .lines()
+        .enumerate()
+        .map(|(y, l)| {
+            l.unwrap()
+                .chars()
+                .enumerate()
+                .map(|(x, number)| Pos(x as i32, y as i32, number as i32))
+                .collect()
+        })
+        .collect();
 
-    let (mut h_x, mut h_y) = (0, 0);
-    let mut visited: HashSet<(i32, i32)> = HashSet::new();
-    visited.insert((0, 0));
+    impl Pos {
+        fn neighbours(&self, g: &Vec<Vec<Pos>>) -> Vec<(Pos, i32)> {
+            let &Pos(x, y, h) = self;
+            let right = g.get(y as usize).and_then(|row| row.get(x as usize + 1));
+            let left = if x - 1 >= 0 {
+                g.get(y as usize).and_then(|row| row.get(x as usize - 1))
+            } else {
+                None
+            };
+            let up = if y - 1 >= 0 {
+                g.get(y as usize - 1).and_then(|row| row.get(x as usize))
+            } else {
+                None
+            };
+            let down = g.get(y as usize + 1).and_then(|row| row.get(x as usize));
 
-    for line in reader.lines() {
-        let instruction = line?;
-        let (direction, steps_count) = instruction.split_at(1);
-        mv(
-            &mut h_x,
-            &mut h_y,
-            direction,
-            steps_count.trim().parse::<i32>().unwrap(),
-        );
-        for i in 0..steps_count.trim().parse::<i32>().unwrap() {
-            
+            println!("{:?} {:?} {:?}", x, y, h);
+
+            vec![right, left, up, down]
+                .into_iter()
+                .filter(|maybe_pos| maybe_pos.is_some() && maybe_pos.unwrap().2 <= h + 1)
+                .map(|p| (p.unwrap().clone(), p.unwrap().2))
+                .collect()
         }
-
-        println!("{} {}", h_x, h_y)
     }
 
-    Ok(println!("{:?}", visited))
-}
+    let result = dijkstra(&Pos(0, 0, 97), |p| p.neighbours(&grid), |p| p.2 == 69 && p);
 
-fn mv(x: &mut i32, y: &mut i32, direction: &str, steps: i32) {
-    match direction {
-        "R" => *x += steps,
-        "L" => *x -= steps,
-        "D" => *y -= steps,
-        "U" => *y += steps,
-        _ => (),
-    }
-}
+    println!("{:?}", grid);
 
-fn catch_up(h_x: &i32, h_y: &i32, t_x: &mut i32, t_y: &mut i32) {
-    if (h_x - *t_x).abs() + (h_y - *t_y).abs() <= 1 {
-        println!("stuck {} {} / {} {}", h_x, h_y, t_x, t_y);
-    }
+    Ok(println!("{:?}", result))
 }
